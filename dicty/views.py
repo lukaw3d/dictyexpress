3ba7ed10080add29f4e05c07a5f5c62a92556b0b
lukaw3d <lukaw3d@gmail.com>
2013-05-18 04:38:40 +0200

    Tidy up

--- dicty/views.py
+++ dicty/views.py
@@ -9,6 +9,7 @@ dictyPath = os.path.abspath(os.path.dirname(__file__))
 dataPath = os.path.join(dictyPath, "../data")
 picklePath = os.path.join(dataPath, 'Pickles')
 
+
 def index(request):
     return render(request, 'dicty/index.html', {})
 
@@ -19,10 +20,10 @@ def others(request, sub):
 
 def api(request, sub):
     #request.GET.get()
-    if sub=="experiment":
+    if sub == "experiment":
         #http://127.0.0.1:8000/api/experiment
         return HttpResponse(json.dumps(wholeDict["experiment"], indent=4))
-    if sub=="profile":
+    if sub == "profile":
         #http://127.0.0.1:8000/api/profile?
         #ddbs=DDB_G0273069%2CDDB_G0279387%2CDDB_G0284861&species=D.%20purpureum
         selectedSpecies = request.GET.get("species")
@@ -31,17 +32,18 @@ def api(request, sub):
         if subset:
             filtered = {sel: subset.get(sel) for sel in selectedDDBs}
             return HttpResponse(json.dumps(filtered, indent=4))
-    if sub=="allGenes":
+        else:
+            return HttpResponse("Hi. Bad params")
+    if sub == "allGenes":
         #http://127.0.0.1:8000/api/allGenes
         return HttpResponse(json.dumps(wholeDict["allGenes"], indent=4))
-    if sub=="comparison":
+    if sub == "comparison":
         #http://127.0.0.1:8000/api/comparison?ddb=DDB_G0273069
         selectedDDB = str(request.GET.get("ddb"))
         filtered = {exp["species"]: {
-            "info": exp, 
+            "info": exp,
             "data": wholeDict["profile"+exp["species"]].get(selectedDDB)
-            } for exp in wholeDict["experiment"]
-        }
+        } for exp in wholeDict["experiment"]}
         return HttpResponse(json.dumps(filtered, indent=4))
     return HttpResponse("Hi")
     #return HttpResponse(json.dumps(wholeDict, indent=4))
@@ -66,7 +68,7 @@ def loadExperiment():
                 header = 0
         f.close()
         cPickle.dump(experiment, open(pickleFile, 'wb'))
-    
+
     experiment = cPickle.load(open(pickleFile, "rb"))
     wholeDict["experiment"] = experiment
     #print json.dumps({"experiment": experiment}, indent=4)
@@ -85,11 +87,12 @@ def loadProfile(folder):
                     splits = line[:-1].split("\t")
                     gene = wholeDict["convertGenes"].get(splits[0])
                     ix = splits[0]
-                    if(gene): ix = gene["ddb"]
+                    if gene:
+                        ix = gene["ddb"]
                     profile[ix] = map(
-                        lambda (x1,x2): (float(x1)+float(x2))/2, 
-                        zip(splits[1:8],splits[8:15])
-                    );
+                        lambda (x1, x2): (float(x1)+float(x2))/2,
+                        zip(splits[1:8], splits[8:15])
+                    )
                 header = 0
         f.close()
         cPickle.dump(profile, open(pickleFile, 'wb'))
@@ -100,8 +103,8 @@ def loadProfile(folder):
 
 
 def loadAllGenes():
-    pickleFile = os.path.join(picklePath,'allGenes.cPickle')
-    dataFile = os.path.join(dataPath,'orthologs.txt')
+    pickleFile = os.path.join(picklePath, 'allGenes.cPickle')
+    dataFile = os.path.join(dataPath, 'orthologs.txt')
     if not os.path.exists(pickleFile):
         f = open(dataFile, 'r')
         header = 1
@@ -133,23 +136,32 @@ def loadAllGenes():
                     }
                 header = 0
         f.close()
-        cPickle.dump((allGenes,convertGenes), open(pickleFile, 'wb'))
+        cPickle.dump((allGenes, convertGenes), open(pickleFile, 'wb'))
 
-    (allGenes,convertGenes) = cPickle.load(open(pickleFile, "rb"))
+    (allGenes, convertGenes) = cPickle.load(open(pickleFile, "rb"))
     wholeDict["allGenes"] = allGenes
     wholeDict["convertGenes"] = convertGenes
     #print json.dumps({"allGenes": allGenes}, indent=4)
 
-if not os.path.exists(dataPath): os.makedirs(dataPath)
-if not os.path.exists(picklePath): os.makedirs(picklePath)
+if not os.path.exists(dataPath):
+    os.makedirs(dataPath)
+if not os.path.exists(picklePath):
+    os.makedirs(picklePath)
 wholeDict = {}
 
-try: loadAllGenes()
-except IOError: print "Data file missing"
-try: loadExperiment()
-except IOError: print "Data file missing"
-try: loadProfile('D. discoideum')
-except IOError: print "Data file missing"
-try: loadProfile('D. purpureum')
-except IOError: print "Data file missing"
-
+try:
+    loadAllGenes()
+except IOError:
+    print "Data file missing"
+try:
+    loadExperiment()
+except IOError:
+    print "Data file missing"
+try:
+    loadProfile('D. discoideum')
+except IOError:
+    print "Data file missing"
+try:
+    loadProfile('D. purpureum')
+except IOError:
+    print "Data file missing"
--- misc files/parseExperimentAsExperiment.py
+++ misc files/parseExperimentAsExperiment.py
@@ -1,7 +1,7 @@
 '''
 experiment.txt in this format, to json
 
-#comments .. 
+#comments ..
 id0	Species	id1	Strain	id2	Growth	org
 D. discoideum    AX4    KA    D. discoideum    AX4    KA
     dd    405894.347598    0.0
@@ -12,7 +12,8 @@ import json
 import cPickle
 import os
 
-if not os.path.exists('Pickles'): os.makedirs('Pickles')
+if not os.path.exists('Pickles'):
+    os.makedirs('Pickles')
 pickleFile = 'Pickles/experiment.cPickle'
 if not os.path.exists(pickleFile):
     f = open('experiment.txt', 'r')
@@ -30,7 +31,6 @@ if not os.path.exists(pickleFile):
             header = 0
     f.close()
     cPickle.dump(experiment, open(pickleFile, 'wb'))
-    
+
 experiment = cPickle.load(open(pickleFile, "rb"))
 print json.dumps({"experiment": experiment}, indent=4)
-
